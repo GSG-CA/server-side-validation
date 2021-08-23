ab94717664ddc4542025f824abe01b85a4ba0a72
Muhammad Abdulhadi <mu7ammadabed@gmail.com>
2021-08-24 02:27:35 +0300

    Add Async solution

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,17 +6,22 @@ const signupSchema = require('../validation/signup-schema');
 const error = require('./error');
 
 router.post('/login', (req, res) => {
-  // Write your code here
   const {error, value} = loginSchema.validate(req.body);
+  
   if (error) {
     res.status(400).json({msg: error.message})
   } else {
     res.json(value)
   }
+
+    // Async using Promises
+  
+  // loginSchema.validateAsync(req.body)
+  // .then(value => res.json(value))
+  // .catch(({message}) => res.status(400).json({msg: message}));
 });
 
 router.post('/signup', (req, res) => {
-  // Write your code here
   const {error, value} = signupSchema.validate(req.body);
 
   if (error) {
@@ -24,6 +29,12 @@ router.post('/signup', (req, res) => {
   } else {
     res.json(value)
   }
+
+  // Async using Promises
+
+  // signupSchema.validateAsync(req.body)
+  // .then(value => res.json(value))
+  // .catch(({message}) => res.status(400).json({msg: message}));
 });
 
 router.use(error.client);
